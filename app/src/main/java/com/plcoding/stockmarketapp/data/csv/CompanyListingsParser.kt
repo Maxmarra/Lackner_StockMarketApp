0939a9dc201bb005b93ac72e420203d2db7bf0a9
Maxmarra <5919669@gmail.com>
2022-08-02 18:15:09 +0300

    04_CSV_Parsing

--- app/src/main/java/com/plcoding/stockmarketapp/data/csv/CompanyListingsParser.kt
+++ app/src/main/java/com/plcoding/stockmarketapp/data/csv/CompanyListingsParser.kt
@@ -15,14 +15,21 @@ class CompanyListingsParser @Inject constructor(): CSVParser<CompanyListing> {
     override suspend fun parse(stream: InputStream): List<CompanyListing> {
 
         val csvReader = CSVReader(InputStreamReader(stream))
+
         return withContext(Dispatchers.IO) {
             csvReader
                 .readAll()
+                    //откидываем первую строку, которая состоит из заголовков
                 .drop(1)
+                    //получаем доступ к каждой строке файла
+                    //присваиваем инфу в первых трех колонках
+                    //соответствующим полям
                 .mapNotNull { line ->
                     val symbol = line.getOrNull(0)
                     val name = line.getOrNull(1)
                     val exchange = line.getOrNull(2)
+                    //переводим все в объект класса CompanyListing
+                    //и возвращаем в итоге List<CompanyListing> таких объектов
                     CompanyListing(
                         name = name ?: return@mapNotNull null,
                         symbol = symbol ?: return@mapNotNull null,
